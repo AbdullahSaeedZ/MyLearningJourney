573709a0fca16693e669fc014ed061386dd8392b
asz14 <asz14@OVER>
2025-04-25 11:11:15 +0300

    Add project files.

---
+++ Variables/Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int MyAge = 27;
+	cout << MyAge << endl;
+
+	MyAge = 30;
+	cout << "my age is " << MyAge <<" Years old" << endl;
+
+	return 0;
+
+
+}
