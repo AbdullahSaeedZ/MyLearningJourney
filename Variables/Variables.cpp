1935198a4675abd699b932cc30994fd137c4cbfb
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-04-25 11:17:50 +0300

    Update Variables.cpp

--- Variables/Variables.cpp
+++ Variables/Variables.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main()
 {
-
+	string MyName = "Abdullah Alzahrani";
 	int MyAge = 27;
-	cout << MyAge << endl;
+	string MyJob = "programmer";
+	int Number = 23;
+	float FloatNumber = 27.5;
+	double NumberWithDecimels = 27.5453345;
+	char MyLetter = 'A';
+	bool boolean = true;
 
-	MyAge = 30;
-	cout << "my age is " << MyAge <<" Years old" << endl;
+	cout << "hi there! my name is " << MyName << ",im " << MyAge << " years old " << "and im a " << MyJob << endl;
 
 	return 0;
-
 
 }
