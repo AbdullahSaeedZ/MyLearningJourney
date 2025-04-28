e1389d0bcf6787cadba16853e4d562c04af2129b
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-04-28 22:35:25 +0300

    update

--- Variables/Variables.cpp
+++ Variables/Variables.cpp
@@ -10,9 +10,21 @@ int main()
 	float FloatNumber = 27.5;
 	double NumberWithDecimels = 27.5453345;
 	char MyLetter = 'A';
-	bool boolean = true;
+	bool Boolean = true;
 
-	cout << "hi there! my name is " << MyName << ",im " << MyAge << " years old " << "and im a " << MyJob << endl;
+	cout << "hi there! my name is " << MyName << ", im " << MyAge << " years old " << "and im a " << MyJob << endl;
+
+	cout << MyAge << endl;
+
+	int x = 3;
+	int y = 5;
+	int Sum = x + y;
+
+	cout << Sum << endl;
+
+	char char1 = 'A', char2 = 'B', char3 = 'C';
+
+	cout << "letters are = " << char1 << char2 << char3 << " and reverse is = " << char3 << char2 << char1 << endl;
 
 	return 0;
 
