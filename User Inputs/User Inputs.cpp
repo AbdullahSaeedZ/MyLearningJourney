72331ae6b414ada3ffff70caaa299da7c1667d3a
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-05-05 03:06:24 +0300

    a

--- User Inputs/User Inputs.cpp
+++ User Inputs/User Inputs.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-using namesapce std;
+using namespace std;
 
 int main()
 
 {
 
+	string UserName;  // need to create an empty variable then we store a value from user inputs using cin >> VariableName;
+
+	cout << "Enter you name please:" << endl;
+	cin >> UserName;
+	cout << "Hi " << UserName << endl;
 
 
-
+	return 0;
 }
