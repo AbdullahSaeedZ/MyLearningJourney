adda09a23a6349005d183d71ffe22d57383191da
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-05-25 23:00:48 +0300

    new

---
+++ Inputs HW3/Inputs HW3/Inputs HW3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int Age;
+
+	cout << "Enter Your Age:";
+	cin >> Age;
+
+	int FutureAge = Age + 5;
+
+	cout << "After 5 years you will be " << FutureAge << " years old." << endl;
+
+
+	return 0;
+}
