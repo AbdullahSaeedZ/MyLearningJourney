5602d879351fa57bf04623b8417cb46acc73d6c2
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-05-26 19:49:39 +0300

    #15

---
+++ #15/#15.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	short int A;
+	short int B;
+
+	cout << "plaese enter the value of A:" << endl;
+	cin >> A;
+	cout << "Please enter the value of B:" << endl;
+	cin >> B;
+
+	short int Area = A * B;
+
+	cout << "The Area of rectangle is: " << Area << endl;
+
+
+	return 0;
+}
+
