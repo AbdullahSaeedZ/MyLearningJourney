47495a38e9ff380a0ed20279463da629a7d9deff
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-05-26 20:07:49 +0300

    #20

---
+++ #20/#20.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+	float A;
+
+	cout << "Enter the Area:" << endl;
+	cin >> A;
+
+	float Area = (3.141592653589793 * A * A) / 4;
+	
+	cout << " the circle area is: " << Area << endl;
+
+
+
+	return 0;
+}
