8915ffbe98641b81e0dc2afa5c69d4d239a4b9ef
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-02 16:36:07 +0300

    rational operators

---
+++ Relational Operators/Relational Operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short int A = 10, B = 20;
+
+    // rational operators are used for comarison of variables as the following:
+
+    cout << (A == B) << endl;
+    cout << (A != B) << endl;
+    cout << (A > B) << endl;
+    cout << (A < B) << endl;
+    cout << (A >= B) << endl;
+    cout << (A <= B) << endl;
+
+    // output is true(1) or false(0) depending on operator used
+    
+    
+    
+    return 0;
+}
+
