6eb2f3dd44e90e79cd639cbb77eaffc5a699cc03
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-03 18:31:57 +0300

    updated

--- #21/#21.cpp
+++ #21/#21.cpp
@@ -9,9 +9,9 @@ int main()
 	cout << "Enter L to calculate circle area along the circumference" << endl;
 	cin >> L;
 
-	float Area = (L * L) / (4 * 3.141592653589793);
+	float Area = pow(L, 2) / (4 * 3.141592653589793);
 
-	cout << " Area is: " << Area << endl;
+	cout << " Area is: " << floor(Area) << endl; // using floor() function to round to the lowest number.
 
 
 
