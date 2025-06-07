81e892d2a289228002d9d9281dc407e487f48d0e
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-07 18:14:10 +0300

    updated

--- #20/#20.cpp
+++ #20/#20.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+void ReadInputs(float& A)
+{
+	cout << "Enter the Area:" << endl;
+	cin >> A;
+}
+
+float CalculateArea(float A)
+{
+	float Area = (3.141592653589793 * pow(A, 2)) / 4;
+	return Area;
+}
+
 int main()
 {
   
 	float A;
-
-	cout << "Enter the Area:" << endl;
-	cin >> A;
-
-	float Area = (3.141592653589793 * pow(A, 2)) / 4;
+	ReadInputs(A);
 	
-	cout << " the circle area is: " << ceil(Area) << endl;
-
-
+	cout << " the circle area is: " << ceil(CalculateArea(A)) << endl;
 
 	return 0;
 }
