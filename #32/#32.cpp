273016500cfaaec41c11dfdd37af711647b2a482
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-07 20:15:35 +0300

    best example of functions

--- #32/#32.cpp
+++ #32/#32.cpp
@@ -2,16 +2,30 @@
 #include <cmath>
 using namespace std;
 
+void ReadInputs(float& Num, float& P)
+{
+    cout << "enter a number and the power: " << endl;
+    cin >> Num >> P;
+}
+
+float PowerOf(float Number, float Power)
+{
+    float Result = pow(Number, Power);
+
+    return Result;
+}
+
+void ShowResult(float Number, float Power)
+{
+    cout << "the result is: " << round(PowerOf(Number, Power)) << endl;
+}
+
 int main()
 {
     
-    short int Number, Power;
-    cout << "enter a number and the power: " << endl;
-    cin >> Number >> Power;
-
-    float Result = pow(Number, Power);
-    cout << "the result is: " << round(Result) << endl;
-
+    float Number, Power;
+    ReadInputs(Number, Power);
+    ShowResult(Number, Power);
 
 
     return 0;
