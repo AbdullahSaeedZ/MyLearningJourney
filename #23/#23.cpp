f6a1731802d1b76cb0bd0b8ec0f1a854ff5371bf
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-07 18:33:59 +0300

    updated

--- #23/#23.cpp
+++ #23/#23.cpp
@@ -2,17 +2,28 @@
 #include <cmath>
 using namespace std;
 
+void ReadInputs(float& A, float& B, float& C)
+{
+    cout << " enter vaules of A, B, C:" << endl;
+    cin >> A >> B >> C;
+}
+
+float CalculateArea(float a, float b, float c)
+{
+    float p = (a + b + c) / 2;
+    float Area = 3.14 * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2);
+    
+    return Area;
+}
+
+
 int main()
 {
     
-    short int a, b, c;
-    cout << " enter vaules of A, B, C:" << endl;
-    cin >> a >> b >> c;
+    float a, b, c;
+    ReadInputs(a, b, c);
 
-    float p = (a + b + c) / 2;
-    float Area = 3.14 * pow((a * b * c) / (4 * sqrt(p * (p - a) * (p - b) * (p - c))), 2);
-
-    cout << "area is: " << round(Area) << endl; // using round() function to round to the nearest number.
+    cout << "area is: " << round(CalculateArea(a, b, c)) << endl; // using round() function to round to the nearest number.
 
 
 
