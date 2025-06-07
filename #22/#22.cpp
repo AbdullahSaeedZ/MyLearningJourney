961d03deab9d0bff74c1ad733f4bd2cd0c822a60
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-07 18:20:18 +0300

    updated

--- #22/#22.cpp
+++ #22/#22.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void ReadInputs(float& A, float& B)
+{
+	cout << "please enter A then B: " << endl;
+	cin >> A;
+	cin >> B;
+}
+
+float CalculateArea(float A, float B)
+{
+	float Area = 3.141592653589793 * (pow(B, 2) / 4) * ((2 * A - B) / (2 * A + B));
+	return Area;
+}
+
 int main()
 {
   
 	float A, B;
+	ReadInputs(A, B);
 
-	cout << "please enter A then B: " << endl;
-	cin >> A;
-	cin >> B;
-
-	float Area = 3.141592653589793 * (pow(B, 2) / 4) * ((2 * A - B) / (2 * A + B));
-
-	cout << "the area is: " << floor(Area) << endl;
+	cout << "the area is: " << floor(CalculateArea(A, B)) << endl;
 
 	return 0;
 }
