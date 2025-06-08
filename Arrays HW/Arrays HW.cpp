6ec278860b4ed4f0be66cfe1b3f58c0fed0d5ded
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-08 22:28:13 +0300

    n

---
+++ Arrays HW/Arrays HW.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void ReadInputs(float (&Grade)[3])  // to use by reference, we use (&Grade)[3] to tell him that there is an array called Grade and has 3 elements
+                                    // if we dont put brackets, he will look for the third elemnt in Grade array.
+{
+	cout << "Enter first grade: " << endl;
+	cin >> Grade[0];
+	cout << "Enter second grade: " << endl;
+	cin >> Grade[1];
+	cout << "Enter third grade: " << endl;
+	cin >> Grade[2];
+	cout << endl; 
+}
+
+float Average(float Grade[3])
+{
+	return (Grade[0] + Grade[1] + Grade[2]) / 3;
+}
+
+int main()
+{
+	float Grade[3];
+	ReadInputs(Grade);
+
+	cout << "The average of grades is: " << Average(Grade) << endl;
+
+
+	return 0;
+
+}
