98056d90e4d4933389e33848f0565dccfa2a5234
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-09 23:55:50 +0300

    done with enums and switch for menu option

--- #44/#44.cpp
+++ #44/#44.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
 using namespace std;
 
+enum enWeek { Sunday =1, Monday=2, Tuesday=3, Wednseday=4, Thursday=5, Friday=6, Saturday=7};
+
 void ReadDay(short int& Day)
 {
-    cout << "Enter the number of day in a week: " << endl;
+    cout << "Choose which day of the week: " << endl << endl;
+    cout << "(1)First day of the week" << endl;
+    cout << "(2)Second day of the week" << endl;
+    cout << "(3)Third day of the week" << endl;
+    cout << "(4)Fourth day of the week" << endl;
+    cout << "(5)Fifth day of the week" << endl;
+    cout << "(6)Sixth day of the week" << endl;
+    cout << "(7)Seventh day of the week" << endl;
     cin >> Day;
 }
 
 void PrintDay(short int Day)
 {
-    if (Day == 1)
+
+    enWeek InputDay = (enWeek)Day;
+
+    switch (InputDay)
     {
-        cout << "It is Sunday!" << endl;
-    }
-    else if (Day == 2)
-    {
-        cout << "It is Monday!" << endl;
-    }
-    else if (Day == 3)
-    {
-        cout << "It is tuesday!" << endl;
-    }
-    else if (Day == 4)
-    {
-        cout << "It is Wednseday!" << endl;
-    }
-    else if (Day == 5)
-    {
-        cout << "It is Thursday!" << endl;
-    }
-    else if (Day == 6)
-    {
-        cout << "It is Friday!" << endl;
-    }
-    else if (Day == 7)
-    {
-        cout << "It is Sunday!" << endl;
-    }
-    else
-    {
-        cout << "Wrong Number, Try Again!" << endl;
+    case enWeek::Sunday: cout << "It is Sunday!" << endl;
+        break;
+    case enWeek::Monday:cout << "It is Monday!" << endl;
+        break;
+    case enWeek::Tuesday:cout << "It is Tuesday!" << endl;
+        break;
+    case enWeek::Wednseday: cout << "It is Wednseday!" << endl;
+        break;
+    case enWeek::Thursday:  cout << "It is Thursday!" << endl;
+        break;
+    case enWeek::Friday: cout << "It is Friday!" << endl;
+        break;
+    case enWeek::Saturday:cout << "It is Saturday!" << endl;
+        break;
+    default: cout << "Wrong Number, Try Again!" << endl;
+
     }
 }
+    
 
 int main()
 {
