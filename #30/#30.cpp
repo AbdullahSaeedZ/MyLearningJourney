05776cfd324524e89aee9b79a6f30e5a5d8ebc47
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-11 02:12:07 +0300

    good counter problem using For loop

---
+++ #30/#30.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int ReadInput()
+{
+    int Input;
+    cout << "Enter a number to calculate its factorail:" << endl;
+    cin >> Input;
+    cout << endl;
+
+    return Input;
+}
+
+int GetFactorial(int Input)
+{
+    int Sum = Input;
+    int Counter = --Input;
+
+    for (Counter; Counter >= 1; Counter--)
+    {
+        Sum = Sum * Counter;
+       
+    }
+
+    return Sum;
+}
+
+
+
+
+int main()
+{
+    int UserInput = ReadInput();
+    cout << GetFactorial(UserInput) << endl;
+}
+
