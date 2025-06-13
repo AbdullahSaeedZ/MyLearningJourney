0b8257663fbba0f327a793c5d05babe2b46d7b3e
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-13 06:44:33 +0300

    using while loop and If-Else

---
+++ #50 using while loop/#50 using while loop.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void ReadPin(int& Pin)
+{
+	cout << "Please Enter you PIN Code to check your balance: " << endl;
+	cin >> Pin;
+
+
+	int Counter = 1;
+
+	
+		while (Counter != 3 && Pin != 1234)
+		{
+			cout << "Wrong! enter PIN again: " << endl;
+			cin >> Pin;
+			Counter++;
+		}
+	
+
+	if (Counter == 3 && Pin != 1234)
+	{
+		cout << "Card is blocked!!" << endl;
+	}
+	else
+	{
+		cout << "Your Balance is: 7500" << endl;
+	}
+
+
+	
+}
+
+
+
+int main()
+{
+	int PIN;
+	ReadPin(PIN);
+
+
+
+	return 0;
+}
+
