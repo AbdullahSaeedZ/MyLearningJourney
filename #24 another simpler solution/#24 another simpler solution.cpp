0cd7d069ccec5adfd02899c2a89f8eb3aced3401
AbdullahSaeedZ <asz1095136568@gmail.com>
2025-06-16 10:02:21 +0300

    nice use of bool function and IF

--- #24 another simpler solution/#24 another simpler solution.cpp
+++ #24 another simpler solution/#24 another simpler solution.cpp
@@ -10,14 +10,14 @@ int ReadAge()
     return Age;
 }
 
-bool CheckAgeValidity(int Age, int From, int To)
+bool CheckAgeValidity(int Age, int From, int To)   // this function can be used any where to validate a number in range for any purpose in any program (reusability)
 {
     return (Age >= From && Age <= To);
 }
 
 void PrintValidity(int Age)
 {
-    if (CheckAgeValidity(Age, 18, 45))
+    if (CheckAgeValidity(Age, 18, 45)) 
         cout << Age << " is a Valid Age!" << endl;
     else
         cout << Age << " is an Invalid Age!" << endl;
