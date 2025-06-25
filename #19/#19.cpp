f26a5446e07520e208ad99296b686411c4ce01dc
Over <asz1095136568@gmail.com>
2025-06-25 18:49:17 +0300

    generating random numbers

---
+++ #19/#19.cpp
@@ -0,0 +1,30 @@
+#include <iostream>   
+#include <string>     
+#include <cstdlib>    // Include cstdlib for rand() and srand() functions.
+#include <ctime>      // Include ctime for the time() function.
+
+using namespace std;  
+
+
+int RandomNumber(int From, int To)
+{
+    // Generate a random number between 0 and (To - From) using rand(),
+    
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;  
+}
+
+int main() {
+    
+    // This ensures that we get a different sequence of random numbers on each run.
+    // this is like a delcaration to start using the random function.
+    // we have to declare it in the main function to generate random values each run, if we declare it in other functions than the main, it will generate the same numbers every time.
+    srand((unsigned)time(NULL));
+
+    
+    cout << RandomNumber(1, 10) << endl;
+    cout << RandomNumber(1, 10) << endl;
+    cout << RandomNumber(1, 10) << endl;
+
+    return 0;  
+}
