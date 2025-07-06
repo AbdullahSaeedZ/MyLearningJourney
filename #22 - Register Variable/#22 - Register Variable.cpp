a8555be5171ed0a75eea896b2dc57bbb1517c998
Over <asz1095136568@gmail.com>
2025-07-06 19:06:51 +0300

    ,

---
+++ #22 - Register Variable/#22 - Register Variable.cpp
@@ -0,0 +1,7 @@
+type of Memories :
+1 - Hard_desk(slow)
+2 - Ram(fast)
+3 - cache memory(faster)
+4 - Register(The fastest and the closest memory to CPU)
+
+Register variable used to be saved in the registers for more speed but it is very sensitive so now its not used anymore in C++
