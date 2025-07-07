ec7d3701d3cacf6ffb71f60bffa6658b758f763e
Over <asz1095136568@gmail.com>
2025-07-07 15:49:03 +0300

    formatting

--- #26 - Setw Manipulator/#26 - Setw Manipulator.cpp
+++ #26 - Setw Manipulator/#26 - Setw Manipulator.cpp
@@ -17,8 +17,8 @@ int main()
     cout << "  Code   |               Name             |  Mark   |" << endl;
     cout << "---------|--------------------------------|---------|" << endl;
 
-    // the body:
-    cout << setw(9) << "C101" << "|" << setw(32) << "Intro to programming 1" << "|" << setw(9) << "99" << "|" << endl;
+    // the body , write (right or left) to show string starting from desired side:
+    cout << left << setw(9) << "C101" << "|" << setw(32) << "Intro to programming 1" << "|" << setw(9) << "99" << "|" << endl;
     cout << setw(9) << "C155" << "|" << setw(32) << "Intro to programming 2" << "|" << setw(9) << "65" << "|" << endl;
     cout << setw(9) << "C123" << "|" << setw(32) << "Intro to programming 3" << "|" << setw(9) << "88" << "|" << endl;
     cout << setw(9) << "C143" << "|" << setw(32) << "Intro to programming 4" << "|" << setw(9) << "65" << "|" << endl;
