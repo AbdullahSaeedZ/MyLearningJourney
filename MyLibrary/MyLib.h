993dbb6036f08ba264a05bf5ea3ac46730f4e087
Over <asz1095136568@gmail.com>
2025-07-12 22:13:24 +0300

    updated

--- MyLibrary/MyLib.h
+++ MyLibrary/MyLib.h
@@ -602,4 +602,26 @@ namespace MyLib
 
     }
 
+    void LoadVectorToNewFile(string FilePath, const vector <string>& VectorContent)
+    {
+        fstream File;
+        File.open(FilePath, ios::out); // ios::out  to create a file if not created.
+
+        if (File.is_open())
+        {
+            for (const string& Line : VectorContent)
+            {
+                if (Line != "")   //to ignore if vector element is empty (due to capacity nature)
+                {
+                    File << Line << endl;
+                }
+            }
+
+            File.close();
+        }
+    }
+
+
+
+
 }
