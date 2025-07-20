862dbe7c577e845bb8901d8445684f3d32d40eb1
Over <asz1095136568@gmail.com>
2025-07-20 17:13:29 +0300

    d

--- MyLibrary/MyLib.h
+++ MyLibrary/MyLib.h
@@ -200,6 +200,19 @@ namespace MyLib
         return ((v == 'a') || (v == 'e') || (v == 'i') || (v == 'o') || (v == 'u'));
     }
 
+    short CountVowelInString(string str)
+    {
+        short count = 0;
+
+        for (short i = 0; i < str.length(); i++)
+        {
+            if (IsVowel(str[i]))
+                count++;
+        }
+
+        return count;
+    }
+
     short ReadPositiveNumInRange(string Message, short From, short To)
     {
         int Number = 0;
