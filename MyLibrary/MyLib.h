a776b5fe8fe6cc0f3dfd6925d80067fe3e381d97
Over <asz1095136568@gmail.com>
2025-07-23 15:35:22 +0300

    d

--- MyLibrary/MyLib.h
+++ MyLibrary/MyLib.h
@@ -332,21 +332,6 @@ namespace MyLib
         double AcctBalance;
     };
 
-    stClientData ReadClientData()
-    {
-        stClientData Client;
-        cout << "Please fill Client data:\n" << endl;
-
-        Client.ID = ReadString("Enter Account ID:");
-        Client.PINCode = ReadString("Enter Pin Code:");
-        Client.Name = ReadString("Enter Name:");
-        Client.Phone = ReadString("Enter Phone Number:");
-        Client.AcctBalance = ReadPositiveNumber("Enter Account Balance:");
-
-        return Client;
-    }
-
-
     string ConvertRecordToLine(stClientData Client1, string delim = "#//#")
     {
         string str = "";
@@ -382,6 +367,61 @@ namespace MyLib
         cout << "Name      : " << Client.Name << endl;
         cout << "Phone     : " << Client.Phone << endl;
         cout << "Balance   : " << Client.AcctBalance << endl;
+    }
+
+    stClientData ReadClientData()
+    {
+        stClientData Client;
+        cout << "Enter Account Number? "; // Usage of std::ws will extract all the whitespace character  
+        getline(cin >> ws, Client.ID);
+
+        cout << "Enter PinCode? ";
+        getline(cin, Client.PINCode);
+
+        cout << "Enter Name? ";
+        getline(cin, Client.Name);
+
+        cout << "Enter Phone? ";
+        getline(cin, Client.Phone);
+
+        cout << "Enter AccountBalance? ";
+        cin >> Client.AcctBalance;
+
+        return Client;
+    }
+
+    string ConvertRecordToLine(stClientData Client1, string delim = "#//#")
+    {
+        string str = "";
+
+        str += Client1.ID + delim;
+        str += Client1.Name + delim;
+        str += Client1.PINCode + delim;
+        str += Client1.Phone + delim;
+        str += to_string(Client1.AcctBalance);
+
+        return str;
+    }
+
+    void AddDataLineToFile(string FilePath, string strDataLine)
+    {
+        fstream DataBase;
+
+        DataBase.open(FilePath, ios::out | ios::app);
+
+        if (DataBase.is_open())
+        {
+            DataBase << strDataLine << endl;
+            DataBase.close();
+        }
+       
+    }
+
+    void AddNewClient(string FilePath)
+    {
+        stClientData Client = ReadClientData();
+
+        AddDataLineToFile(FilePath, ConvertRecordToLine(Client, "#//#"));
     }
 
     short ReadPositiveNumInRange(string Message, short From, short To)
