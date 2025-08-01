59a9dc5abf92f75776b3e6ac5c1a1f7bafea8759
Over <asz1095136568@gmail.com>
2025-08-01 04:07:17 +0300

    done

---
+++ #15 -  Is last day in month - Is last month in year/#15 -  Is last day in month - Is last month in year.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+struct stDate {
+	short month = 0, day = 0, year = 0;
+};
+
+int ReadPositiveNumber(string Message)
+{
+	int Number = 0;
+	do
+	{
+		cout << Message << endl;
+		cin >> Number;
+
+		if (cin.fail())
+		{
+			// if other than numbers entered system will fail.
+			cin.clear();                // to clear the failure
+			cin.ignore(10000, '\n');    // to ignore what was entered before, to clean the buffer
+			cout << "Invalid input! Please enter a number." << endl;
+			continue;
+		}
+
+	} while (Number <= 0);
+
+	return Number;
+}
+
+stDate ReadDate()
+{
+	stDate Date;
+	Date.day = ReadPositiveNumber("Enter Day:");
+	Date.month = ReadPositiveNumber("Enter Month:");
+	Date.year = ReadPositiveNumber("Enter year:");
+
+	return Date;
+}
+
+bool IsLeapYear(short num)
+{
+	return ((num % 4 == 0) && (num % 100 != 0)) || ((num % 400 == 0));
+}
+
+int NumOfMonthDays(int year, int mon)
+{
+	return (mon < 1 || mon > 12) ? 0 : (mon == 2) ? ((IsLeapYear(year)) ? 29 : 28) : (mon == 2 || mon == 4 || mon == 6 || mon == 9 || mon == 11) ? 30 : 31;
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+	return (Date.day == NumOfMonthDays(Date.year, Date.month));
+}
+
+bool IsLastMonthInYear(short month)
+{
+	return (month == 12);
+}
+
+int main()
+{
+	stDate Date = ReadDate();
+
+	if (IsLastDayInMonth(Date))
+		cout << "yes, day is last day in month" << endl;
+	else
+		cout << "no, day is not last day in month" << endl;
+
+	if (IsLastMonthInYear(Date.month))
+		cout << "yes, month is last month in year" << endl;
+	else
+		cout << "no, month is not last month in year" << endl;
+
+
+    return 0;
+}
+
