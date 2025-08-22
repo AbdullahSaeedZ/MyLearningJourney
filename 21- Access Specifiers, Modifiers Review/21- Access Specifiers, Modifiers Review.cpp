570a4628a4592d1c6271f0ce37c3792794bfebfb
Over <asz1095136568@gmail.com>
2025-08-22 22:20:42 +0300

    done

---
+++ 21- Access Specifiers, Modifiers Review/21- Access Specifiers, Modifiers Review.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+
+class clsA
+{
+private: 
+    int APrivateVar;
+
+
+protected:
+    int AProtectedVar;
+    string ProtectedNameFunction()
+    {
+        return "Protected Function";
+    }
+
+public:
+
+    int APublicVar;
+    string PublicEmailFunction()
+    {
+        return "Public Email Function";
+    }
+};
+
+
+class clsB : public clsA
+{
+
+
+public:
+
+    // the sub class B can access its own members + class A protected and public members.
+    // protected members of class A can be accessed inside the class B, not by any objects, only classes.
+    // accessing is done by using escape scope.
+
+    void PublicTestFunction()
+    {
+        cout << clsA::ProtectedNameFunction << endl;
+    }
+
+    int BPublicVar;
+
+};
+
+
+int main()
+{
+    clsB B;
+
+    // B object can ONLY access all PUBLIC members of base and sub class;
+    B.
+
+
+
+    return 0;
+}
