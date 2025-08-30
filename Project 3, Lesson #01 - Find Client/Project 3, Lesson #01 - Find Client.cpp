9f3ee2ae7a6389f7fc9de9cdd623242ef1511650
Over <asz1095136568@gmail.com>
2025-08-30 14:19:21 +0300

    done

--- Project 3, Lesson #01 - Find Client/Project 3, Lesson #01 - Find Client.cpp
+++ Project 3, Lesson #01 - Find Client/Project 3, Lesson #01 - Find Client.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include "clsBankClient.h"
 using namespace std;
 
 int main()
 {
     
+    clsBankClient Client1 = clsBankClient::Find("A100");
 
+    Client1.Print();
 
+    cout << clsBankClient::IsClientExist("A10043");
 
     return 0;
 }
