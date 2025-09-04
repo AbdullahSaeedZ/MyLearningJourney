b6b54f1fa90814510905ee2a3f44d879370e31c5
Over <asz1095136568@gmail.com>
2025-09-04 22:18:02 +0300

    done

---
+++ Project 3 - UI - Transactions Menu - Lesson 15 - Withdraw Screen/clsScreen.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+using namespace std;
+
+
+class clsScreen
+{
+
+protected:
+
+    static void _ReadClientInfo(clsBankClient& Client)
+    {
+        Client.FirstName = clsInputValidate::ReadString("\nEnter First Name: ");
+        Client.LastName = clsInputValidate::ReadString("\nEnter Last Name: ");
+        Client.Email = clsInputValidate::ReadString("\nEnter Email: ");
+        Client.PhoneNumber = clsInputValidate::ReadString("\nEnter Phone Number: ");
+        Client.PinCode = clsInputValidate::ReadString("\nEnter Pin Code: ");
+        Client.AcctBalance = clsInputValidate::ReadFloatNumber("\nEnter Balance:");
+    }
+
+
+    // below are common function in more than one derived screen classes like update - delete - add client, so i put it here for fast use and in case of any change later will be done once here.
+
+    static void _PrintClientInfo(clsBankClient &Client)
+    {
+        cout << "\nClient Card:" << endl;
+        cout << "____________________________________" << endl;
+        cout << "Acct. Number: " << Client.getAcctNumber() << endl;
+        cout << "First Name  : " << Client.FirstName << endl; // using property declaration
+        cout << "Last Name   : " << Client.LastName << endl;
+        cout << "Full Name   : " << Client.getFullName() << endl;
+        cout << "Email       : " << Client.Email << endl;
+        cout << "Phone       : " << Client.PhoneNumber << endl;
+        cout << "Balance     : " << Client.AcctBalance << " SAR" << endl;
+        cout << "Password    : " << Client.PinCode << endl;
+        cout << "____________________________________\n" << endl;
+    }
+
+    static void _DrawScreenHeader(string Title, string SubTitle = "")
+    {
+        cout << "___________________________________________" << endl;
+        cout << Title << endl;
+        if (SubTitle != "")
+        {
+            cout << SubTitle << endl;
+        }
+        cout <<"___________________________________________\n\n";
+    }
+
+  
+
+};
+
