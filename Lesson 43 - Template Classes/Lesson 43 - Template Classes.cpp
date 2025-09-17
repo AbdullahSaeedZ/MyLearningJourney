79845954b95102b3dec9e6bb50e2d4b681856bd8
Over <asz1095136568@gmail.com>
2025-09-17 12:20:44 +0300

    done

--- Lesson 43 - Template Classes/Lesson 43 - Template Classes.cpp
+++ Lesson 43 - Template Classes/Lesson 43 - Template Classes.cpp
@@ -1,14 +1,57 @@
 #include <iostream>
 using namespace std;
 
+// this is the way vectors are made.
 
 
+template <class T>
+class Calculator
+{
+private:
+
+    T Num1;
+    T Num2;
+
+public:
+
+    Calculator(T Number1, T Number2)
+    {
+        Num1 = Number1;
+        Num2 = Number2;
+    }
+
+    T Add()
+    {
+        return Num1 + Num2;
+    }
+
+    T Multiply()
+    {
+        return Num1 * Num2;
+    }
+
+    void PrintResults()
+    {
+        cout << "Number1 : " << Num1 << endl;
+        cout << "Number2 : " << Num2 << endl;
+        cout << "Addition Result: " << Add() << endl;
+    }
+
+
+
+};
 
 
 int main()
 {
     
+    Calculator <int> IntObject(1, 2); 
+
+    Calculator <float> FloatObject(1.5, 2.4);
+
+    IntObject.PrintResults();
     
+    FloatObject.PrintResults();
     
     
     
