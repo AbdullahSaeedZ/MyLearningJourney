9c58f1e3b235455b93f8e4c8eb3c0e12c147f4b6
Over <asz1095136568@gmail.com>
2025-09-17 12:06:02 +0300

    done

---
+++ Lesson 42 - Template Functions/Lesson 42 - Template Functions.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+// in overloading two  functions with same name but different signatures and different code implementation.
+// in template it is on function that takes different data types and do one operation(code implementation)
+
+
+
+template <typename koko> // declare the template, this indicate that we want to take multiple data types under koko variable.
+koko Add(koko Num1, koko Num2) // it take any data type.
+{
+    return Num1 + Num2;
+}
+
+
+int main()
+{
+
+
+    cout << Add<int>(3, 7) << endl;
+    cout << Add<string>("abdullah ", "Alzahrani") << endl;
+    return 0;
+}
+
