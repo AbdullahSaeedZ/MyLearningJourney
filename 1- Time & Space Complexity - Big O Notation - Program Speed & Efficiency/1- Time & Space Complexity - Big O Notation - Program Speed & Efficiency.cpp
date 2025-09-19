7da14967b4ab1e3773d91ba29d75405e88139dd4
Over <asz1095136568@gmail.com>
2025-09-20 00:50:06 +0300

    done

---
+++ 1- Time & Space Complexity - Big O Notation - Program Speed & Efficiency/1- Time & Space Complexity - Big O Notation - Program Speed & Efficiency.cpp
@@ -0,0 +1,25 @@
+﻿ ================== Factors Affecting Program Speed ==================
+
+// 1) Algorithm (complexity)
+    - The way we design and implement algorithms directly impacts speed.
+    - Measured by two main complexities:
+
+       a) Time Complexity → How much time the algorithm takes to run.
+       b) Space Complexity → How much memory (RAM) the algorithm consumes.
+
+ 
+ 
+// 2) Hardware & System Resources (performance)
+    - The physical and system environment where the program runs.
+    - Includes:
+       • CPU (Processor speed, cores, cache).
+       • RAM (Available memory).
+       • Hard drive / SSD speed.
+       • Operating System (efficiency, resource management).
+    - Better hardware = better performance.
+
+ ------------------------------------------------------
+// SUMMARY:
+ - Program Speed depends on both Algorithm + Hardware.
+ - Algorithm → Complexity (Time & Space).
+ - Hardware → Performance (CPU, RAM, OS, etc).
