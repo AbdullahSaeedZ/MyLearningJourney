1b98e2a0d972609a054d597c15c265efed41df53
Over <asz1095136568@gmail.com>
2025-09-28 03:32:40 +0300

    done

---
+++ What is Circular Linked List/What is Circular Linked List.cpp
@@ -0,0 +1,15 @@
+// see pdf
+
+// Singly Circular Linked List:
+// - Each node has: [data | next]
+// - The "next" of the last node points back to the head
+// - There is no NULL pointer in the list
+// - You can traverse forever in one direction (next)
+
+
+// Doubly Circular Linked List:
+// - Each node has: [prev | data | next]
+// - The "next" of the last node points to the head
+// - The "prev" of the head points back to the last node
+// - Traversal can go in both directions (forward and backward)
+// - Also, no NULL pointers (completely circular)
