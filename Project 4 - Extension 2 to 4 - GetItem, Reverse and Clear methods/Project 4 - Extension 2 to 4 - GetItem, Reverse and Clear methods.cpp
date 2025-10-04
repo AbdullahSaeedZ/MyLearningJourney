aff9b5b51e21e59bc2c68c0ae55a5caf6a617c0a
Over <asz1095136568@gmail.com>
2025-10-04 15:26:22 +0300

    done

---
+++ Project 4 - Extension 2 to 4 - GetItem, Reverse and Clear methods/Project 4 - Extension 2 to 4 - GetItem, Reverse and Clear methods.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "clsDynamicArray.h"
+using namespace std;
+
+int main()
+{
+
+    clsDynamicArray <int> DyArr(5);
+
+    DyArr.SetItem(0, 10);
+    DyArr.SetItem(1, 20);
+    DyArr.SetItem(2, 30);
+    DyArr.SetItem(3, 40);
+    DyArr.SetItem(4, 50);
+
+    cout << "Is empty: " << DyArr.IsEmpty() << endl;
+    cout << "Size: " << DyArr.Size() << endl;
+
+    cout << "\narray items: " << endl;
+    DyArr.PrintItems();
+
+   /* cout << "\n\nresize from 5 items to 2: " << endl;
+    DyArr.ReSize(2);
+    DyArr.PrintItems();
+
+    cout << "\n\nresize from 2 items to 10: " << endl;
+    DyArr.ReSize(10);
+    DyArr.PrintItems();*/
+
+    cout << "\n\nValue of index 1: " << DyArr.GetItemValueByIndex(1) << endl;
+
+    cout << "\nitems after reversing:" << endl;
+    DyArr.Reverse();
+    DyArr.PrintItems();
+
+    cout << "\n\nitems after clearing:" << endl;
+    DyArr.Clear();
+
+    return 0;
+}
