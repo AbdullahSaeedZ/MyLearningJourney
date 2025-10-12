e3cad4aba3b9f7cac526e1962800b2f42bb33552
Over <asz1095136568@gmail.com>
2025-10-13 02:12:03 +0300

    done

---
+++ Csharp-Level1/13 - Managed vs Unmanaged Code/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// managed code is any code written with .Net langs under .Net platform and managed by CLR
+// unmanaged code is any code written outside .Net platform. for example when you write a code in java or C++.
+
+// .Net Platform can deal with both managed and unmanaged code.
+// CLR cant control unmanaged code directly, BUT can provide tools to work with unmanaged code. it provides Interoperability layer to allow managed and unmanaged to interoperate.
+
+// .NET provides interoperability features like COM (very old tech, now there is APIs) to communicate with unmanaged code.
+
+
+namespace _13___Managed_vs_Unmanaged_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
