3ddda9ab890eef232724b2de1fc7d81cfe0001b9
Over <asz1095136568@gmail.com>
2025-10-13 01:52:51 +0300

    done

---
+++ Csharp-Level1/12 - CLR Functions/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*******************************************************
+ * ⚙️ .NET CLR Functions
+ *******************************************************/
+
+/*
+ * The CLR (Common Language Runtime) is the core engine , manager for all .Net langs
+ * that manages execution, memory, and safety for .NET apps.
+ */
+
+/*
+ * 1️⃣ Execution & Compilation
+ * -----------------------------------------------------
+ * - Converts IL → native machine code via JIT compiler.
+ * - Optimizes code at runtime for best performance.
+ */
+
+/*
+ * 2️⃣ Memory & Resource Management
+ * -----------------------------------------------------
+ * - Handles memory allocation and automatic Garbage Collection. by Garbage Collectors (GC)
+ * - Prevents leaks and dangling pointers (unlike manual deletion in C++).
+ */
+
+/*
+ * 3️⃣ Type & Exception Handling
+ * -----------------------------------------------------
+ * - Enforces type-safety to avoid invalid operations. by Common Type System (CTS)
+ * - Manages exceptions in a unified, language-independent way.
+ */
+
+/*
+ * 4️⃣ Security & Performance
+ * -----------------------------------------------------
+ * - Verifies assemblies and enforces access permissions.
+ * - Runs code in a safe sandboxed environment.
+ * - Performs runtime optimizations for better speed.
+ */
+
+/*
+ * 5️⃣ Interoperability & Independence
+ * -----------------------------------------------------
+ * - Language-independent → runs code from any .NET language.
+ * - Platform-independent → executes across Windows, Linux, macOS.
+ */
+
+/*
+ * ✅ In short:
+ * -----------------------------------------------------
+ * CLR = The “brain or general manager” of .NET.
+ * It compiles, runs, secures, and manages your program,
+ * turning IL into efficient, safe, native machine code.
+ */
+
+
+namespace _12___CLR_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
