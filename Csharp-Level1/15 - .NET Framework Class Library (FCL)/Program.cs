a074566961eb830e79b4e4f3a8778d3a23df9777
Over <asz1095136568@gmail.com>
2025-10-13 02:35:59 +0300

    SEE PDF FILE

---
+++ Csharp-Level1/15 - .NET Framework Class Library (FCL)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// IMPORTANT !! SEE PDF FILE
+
+
+namespace _15__.NET_Framework_Class_Library__FCL_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
