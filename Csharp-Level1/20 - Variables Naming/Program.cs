b24ee3cf640f6a1680dbc7bba7b47342d137afd7
Over <asz1095136568@gmail.com>
2025-10-13 05:31:51 +0300

    done

---
+++ Csharp-Level1/20 - Variables Naming/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// check the pdf file
+
+
+namespace _20___Variables_Naming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
