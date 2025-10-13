ae8495b937973a5b8b1faf99472d0497b3c2edd9
Over <asz1095136568@gmail.com>
2025-10-13 21:41:50 +0300

    pdf file

---
+++ Csharp-Level1/23 - Predefined Datatypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// check pdf
+
+namespace _23___Predefined_Datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
