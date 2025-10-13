7b6d14d45388d4c74e4fdce63ca47b9c58bcd283
Over <asz1095136568@gmail.com>
2025-10-14 01:04:38 +0300

    done

---
+++ Csharp-Level1/32 - Ticks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _32___Ticks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+                |< -----------------------------------------timeline---------------------------------------- >|
+            1 / 1 / 0001 00:00:00                              now                                     31 / 12 / 9999
+              Ticks = 0                                     Ticks = billions                      Ticks = 3155378975999999999
+            */
+
+
+            // tick is the smallest time unit in C# inside the DateTime struct
+            // Each tick = 100 nanoseconds (0.0000001 sec)
+            // Ticks count from 1 Jan, 0001 (Gregorian calendar)
+
+            Console.WriteLine(DateTime.MinValue.Ticks); // 0 → start of time range
+            Console.WriteLine(DateTime.MaxValue.Ticks); // 3155378975999999999 → end of time range
+
+
+
+
+            // lets see hom much time it takes for a printing method to execute:
+
+            DateTime Start = DateTime.Now;
+            Console.WriteLine("Hello world");
+            DateTime End = DateTime.Now;
+
+            long Duration = End.Ticks - Start.Ticks;
+
+            Console.WriteLine("how many ticks to run printing function: " + Duration);
+
+
+        }
+    }
+}
