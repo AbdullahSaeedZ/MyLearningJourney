d9f474d916fdbe66eb0eaa6e79e17b4a7689e55a
Over <asz1095136568@gmail.com>
2025-10-13 04:34:54 +0300

    done

---
+++ Csharp-Level1/17 - Syntax/Program.cs
@@ -0,0 +1,19 @@
+﻿using System; 
+// System = <iostream> for inputs-outputs
+// using = #include
+
+// since C# is fully OOP, all code must be inside classes
+
+
+
+// namespace is just grouping code under specific name, or here as container grouping classes
+namespace Main
+{
+    internal class Program // can be any name
+    {
+        static void Main(string[] args) // Entry point.  static to call without object
+        {
+          // code
+        }
+    }
+}
