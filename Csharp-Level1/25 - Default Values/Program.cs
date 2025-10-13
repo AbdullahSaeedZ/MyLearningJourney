73cb3196af8ee70f1ef9a4a403251aa5e76f2fbf
Over <asz1095136568@gmail.com>
2025-10-13 22:18:54 +0300

    done

---
+++ Csharp-Level1/25 - Default Values/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _25___Default_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // numeric types have a default 0 value 
+            // bool has a default false value 
+            // char has a default '\0 value 
+
+            // reference types (string, class,...) has a default null value 
+
+            //get default value using default(type)
+            int i = default(int); // 0
+            float f = default(float);// 0
+            decimal d = default(decimal);// 0
+            bool b = default(bool);// false
+            char c = default(char);// '\0'
+
+            // C# 7.1 onwards
+            //get default value using default
+            int i2 = default; // 0
+            float f2 = default;// 0
+            decimal d2 = default;// 0
+            bool b2 = default;// false
+            char c2 = default;// '\0'
+            string s1 = default; // null
+
+            
+
+
+        }
+    }
+}
