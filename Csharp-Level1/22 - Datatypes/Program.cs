a31875937056cfed54974c1d716924de23fa5759
Over <asz1095136568@gmail.com>
2025-10-13 21:21:10 +0300

    pdf file

---
+++ Csharp-Level1/22 - Datatypes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// check pdf file
+
+
+
+namespace _22___Datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
