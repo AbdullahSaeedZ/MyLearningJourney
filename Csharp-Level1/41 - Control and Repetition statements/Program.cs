80c2f9d5ef0e4137db07c3dcbc13fbd374d65c15
Over <asz1095136568@gmail.com>
2025-10-14 23:20:16 +0300

    done

---
+++ Csharp-Level1/41 - Control and Repetition statements/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Same as C++, but in C# conditions must evaluate to a boolean value (true or false).
+// Numeric values like 0 or 1 cannot be used directly as conditions.
+
+
+
+namespace _41___Control_and_Repetition_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
