eb7b9e4db921fd24dc6d9b3bfd68f50a126ef0f8
Over <asz1095136568@gmail.com>
2025-10-15 04:24:57 +0300

    done

---
+++ Csharp-Level1/50 - Random Function In C#/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50___Random_Function_In_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // using Random class then creating an object
+            Random rnd = new Random();
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(rnd.Next(10, 20)); // returns random integers >= 10 and < 20
+            }
+
+            Console.WriteLine();
+
+            // or 
+
+            for (int j = 0; j < 4; j++)
+            {
+                Console.WriteLine(rnd.Next(20)); // returns random integers from 0 to < 20
+            }
+
+        }
+    }
+}
