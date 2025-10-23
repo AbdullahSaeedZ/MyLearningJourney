002f43e52ea1aa6d606d512e7095dc4b6c62a139
Over <asz1095136568@gmail.com>
2025-10-23 20:36:03 +0300

    done

---
+++ Csharp-Level1/65 - Timer/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _65___Timer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        int counter = 0;
+
+        // once the timer is enabled, the interval of 1000 ms (1 sec) will start then at end of interval the tick function will be triggered.
+        // since it is enabled, the intervals will keep running and triggering the tick function
+        // lower the interval time to make the tick event triggers faster
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            label1.Text = counter.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+    }
+}
