1a3945c74c5bea407eccbac9969b0606786b3e9d
Over <asz1095136568@gmail.com>
2025-10-23 20:16:20 +0300

    done

---
+++ Csharp-Level1/64 - MonthCalandar/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _64___MonthCalandar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // we can use all methods of date and time to string 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionStart.ToShortDateString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(monthCalendar1.SelectionRange.End.ToString()); // or this way
+        }
+    }
+}
