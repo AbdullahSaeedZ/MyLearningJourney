b1fe8f9033a67bf573819909f086018a4b13640d
Over <asz1095136568@gmail.com>
2025-10-25 19:17:33 +0300

    done

---
+++ Csharp-Level1/69 - ListView/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace _69___ListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnFillRandom = new System.Windows.Forms.Button();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.clmnID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmnName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbAge = new System.Windows.Forms.TextBox();
+            this.clmnAge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.rbDetails = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbSmallIcons = new System.Windows.Forms.RadioButton();
+            this.rbLargeIcons = new System.Windows.Forms.RadioButton();
+            this.rbTile = new System.Windows.Forms.RadioButton();
+            this.rbList = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmnID,
+            this.clmnName,
+            this.clmnAge});
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList2;
+            this.listView1.Location = new System.Drawing.Point(12, 238);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1384, 482);
+            this.listView1.SmallImageList = this.imageList1;
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "boy.png");
+            this.imageList1.Images.SetKeyName(1, "girl.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "boy.png");
+            this.imageList2.Images.SetKeyName(1, "girl.png");
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(450, 68);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(115, 43);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(312, 68);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(115, 43);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnFillRandom
+            // 
+            this.btnFillRandom.Location = new System.Drawing.Point(586, 68);
+            this.btnFillRandom.Name = "btnFillRandom";
+            this.btnFillRandom.Size = new System.Drawing.Size(115, 43);
+            this.btnFillRandom.TabIndex = 1;
+            this.btnFillRandom.Text = "Fill Random";
+            this.btnFillRandom.UseVisualStyleBackColor = true;
+            this.btnFillRandom.Click += new System.EventHandler(this.btnFillRandom_Click);
+            // 
+            // tbID
+            // 
+            this.tbID.Location = new System.Drawing.Point(65, 35);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(119, 22);
+            this.tbID.TabIndex = 2;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(65, 78);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(225, 22);
+            this.tbName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Name:";
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(65, 141);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(58, 20);
+            this.rbMale.TabIndex = 4;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(174, 141);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(74, 20);
+            this.rbFemale.TabIndex = 4;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // clmnID
+            // 
+            this.clmnID.Text = "ID";
+            this.clmnID.Width = 126;
+            // 
+            // clmnName
+            // 
+            this.clmnName.Text = "Name";
+            this.clmnName.Width = 129;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Age:";
+            // 
+            // tbAge
+            // 
+            this.tbAge.Location = new System.Drawing.Point(65, 106);
+            this.tbAge.Name = "tbAge";
+            this.tbAge.Size = new System.Drawing.Size(108, 22);
+            this.tbAge.TabIndex = 5;
+            // 
+            // clmnAge
+            // 
+            this.clmnAge.Text = "Age";
+            // 
+            // rbDetails
+            // 
+            this.rbDetails.AutoSize = true;
+            this.rbDetails.Location = new System.Drawing.Point(17, 40);
+            this.rbDetails.Name = "rbDetails";
+            this.rbDetails.Size = new System.Drawing.Size(70, 20);
+            this.rbDetails.TabIndex = 7;
+            this.rbDetails.TabStop = true;
+            this.rbDetails.Tag = "Details";
+            this.rbDetails.Text = "Details";
+            this.rbDetails.UseVisualStyleBackColor = true;
+            this.rbDetails.Click += new System.EventHandler(this.rbDetails_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbList);
+            this.groupBox1.Controls.Add(this.rbTile);
+            this.groupBox1.Controls.Add(this.rbLargeIcons);
+            this.groupBox1.Controls.Add(this.rbSmallIcons);
+            this.groupBox1.Controls.Add(this.rbDetails);
+            this.groupBox1.Location = new System.Drawing.Point(937, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 151);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "View";
+            // 
+            // rbSmallIcons
+            // 
+            this.rbSmallIcons.AutoSize = true;
+            this.rbSmallIcons.Location = new System.Drawing.Point(17, 77);
+            this.rbSmallIcons.Name = "rbSmallIcons";
+            this.rbSmallIcons.Size = new System.Drawing.Size(97, 20);
+            this.rbSmallIcons.TabIndex = 7;
+            this.rbSmallIcons.TabStop = true;
+            this.rbSmallIcons.Tag = "Small";
+            this.rbSmallIcons.Text = "Small Icons";
+            this.rbSmallIcons.UseVisualStyleBackColor = true;
+            this.rbSmallIcons.Click += new System.EventHandler(this.rbDetails_Click);
+            // 
+            // rbLargeIcons
+            // 
+            this.rbLargeIcons.AutoSize = true;
+            this.rbLargeIcons.Location = new System.Drawing.Point(17, 113);
+            this.rbLargeIcons.Name = "rbLargeIcons";
+            this.rbLargeIcons.Size = new System.Drawing.Size(98, 20);
+            this.rbLargeIcons.TabIndex = 7;
+            this.rbLargeIcons.TabStop = true;
+            this.rbLargeIcons.Tag = "Large";
+            this.rbLargeIcons.Text = "Large Icons";
+            this.rbLargeIcons.UseVisualStyleBackColor = true;
+            this.rbLargeIcons.Click += new System.EventHandler(this.rbDetails_Click);
+            // 
+            // rbTile
+            // 
+            this.rbTile.AutoSize = true;
+            this.rbTile.Location = new System.Drawing.Point(149, 40);
+            this.rbTile.Name = "rbTile";
+            this.rbTile.Size = new System.Drawing.Size(51, 20);
+            this.rbTile.TabIndex = 7;
+            this.rbTile.TabStop = true;
+            this.rbTile.Tag = "Tile";
+            this.rbTile.Text = "Tile";
+            this.rbTile.UseVisualStyleBackColor = true;
+            this.rbTile.Click += new System.EventHandler(this.rbDetails_Click);
+            // 
+            // rbList
+            // 
+            this.rbList.AutoSize = true;
+            this.rbList.Location = new System.Drawing.Point(149, 77);
+            this.rbList.Name = "rbList";
+            this.rbList.Size = new System.Drawing.Size(48, 20);
+            this.rbList.TabIndex = 7;
+            this.rbList.TabStop = true;
+            this.rbList.Tag = "List";
+            this.rbList.Text = "List";
+            this.rbList.UseVisualStyleBackColor = true;
+            this.rbList.Click += new System.EventHandler(this.rbDetails_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1408, 732);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbAge);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.btnFillRandom);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnFillRandom;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.ColumnHeader clmnID;
+        private System.Windows.Forms.ColumnHeader clmnName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbAge;
+        private System.Windows.Forms.ColumnHeader clmnAge;
+        private System.Windows.Forms.RadioButton rbDetails;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbList;
+        private System.Windows.Forms.RadioButton rbTile;
+        private System.Windows.Forms.RadioButton rbLargeIcons;
+        private System.Windows.Forms.RadioButton rbSmallIcons;
+    }
+}
+
---
+++ Csharp-Level1/69 - ListView/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _69___ListView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tbID.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbID.Text) || string.IsNullOrWhiteSpace(tbName.Text))
+                return;
+
+            // prepare the item before adding to the listView.
+            // item is the ID, then name and other details are sub items
+            ListViewItem item = new ListViewItem(tbID.Text.Trim());  // this is an item, and the details of this item are sub items.
+
+            if (rbMale.Checked)
+                item.ImageIndex = 0;
+            else
+                item.ImageIndex = 1;
+
+            // adding subitems will show data on columns based on the adding order.
+            item.SubItems.Add(tbName.Text.Trim());
+            item.SubItems.Add(tbAge.Text.Trim());
+
+            // now item is ready to be added to the listView
+            listView1.Items.Add(item);
+
+            //reset text boxes for new entry
+            tbID.Clear();
+            tbName.Clear();
+            tbID.Focus(); // but curser focus on this text box.
+
+            
+        }
+
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count > 0)
+            {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+        }
+
+        private void btnFillRandom_Click(object sender, EventArgs e)
+        {
+            for (byte i = 1; i <= 10; i++)
+            {
+
+                ListViewItem item = new ListViewItem(i.ToString());
+                if (i % 2 == 0)
+                    item.ImageIndex = 0;
+                else
+                    item.ImageIndex = 1;
+
+                item.SubItems.Add("random");
+
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(listView1.SelectedItems[0].Text);
+        }
+
+        private void rbDetails_Click(object sender, EventArgs e)
+        {
+            RadioButton button = (RadioButton)sender;
+
+            switch (button.Tag.ToString())
+            {
+                case "Details": listView1.View = View.Details; break;
+                case "Large": listView1.View = View.LargeIcon; break;
+                case "Small": listView1.View = View.SmallIcon; break;
+                case "List": listView1.View = View.List; break;
+                case "Tile": listView1.View = View.Tile; break;
+            }
+
+            listView1.Refresh();
+        }
+    }
+}
