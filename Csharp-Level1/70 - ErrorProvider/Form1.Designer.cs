7447592d0173bebe8eb77ad98a36d89b5edaf07c
Over <asz1095136568@gmail.com>
2025-10-25 19:48:32 +0300

    done

---
+++ Csharp-Level1/70 - ErrorProvider/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace _70___ErrorProvider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFirstName = new System.Windows.Forms.TextBox();
+            this.tbMidName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLastName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "first name";
+            // 
+            // tbFirstName
+            // 
+            this.tbFirstName.Location = new System.Drawing.Point(154, 65);
+            this.tbFirstName.Name = "tbFirstName";
+            this.tbFirstName.Size = new System.Drawing.Size(207, 22);
+            this.tbFirstName.TabIndex = 1;
+            this.tbFirstName.Validating += new System.ComponentModel.CancelEventHandler(this.tbFirstName_Validating);
+            // 
+            // tbMidName
+            // 
+            this.tbMidName.Location = new System.Drawing.Point(154, 107);
+            this.tbMidName.Name = "tbMidName";
+            this.tbMidName.Size = new System.Drawing.Size(207, 22);
+            this.tbMidName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "middle name";
+            // 
+            // tbLastName
+            // 
+            this.tbLastName.Location = new System.Drawing.Point(154, 144);
+            this.tbLastName.Name = "tbLastName";
+            this.tbLastName.Size = new System.Drawing.Size(207, 22);
+            this.tbLastName.TabIndex = 5;
+            this.tbLastName.Validating += new System.ComponentModel.CancelEventHandler(this.tbLastName_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "last name";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(443, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(446, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "link the textbox to the error provider by the Validating property in the textbox";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(921, 318);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbLastName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbMidName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbFirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFirstName;
+        private System.Windows.Forms.TextBox tbMidName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLastName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Csharp-Level1/70 - ErrorProvider/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _70___ErrorProvider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // one error provider can be used on multiple text boxes
+        private void tbFirstName_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbFirstName.Text))
+            {
+                // cancel means cancel the moving event or prevent from moving to other events till we set e.cancel to false
+                e.Cancel = true;  // this will not allow the user get out of the textbox till he type needed text, and will keep focus on the text box
+
+                //                      control used         error message on tooltip (when u hover on the error icon)
+                errorProvider1.SetError(tbFirstName, "First Name must have a value!");
+            }
+            else
+            {
+                e.Cancel = false; // this will allow user to leave the text box
+                errorProvider1.SetError(tbFirstName, ""); // means clear the error icon and tooltip for that control.
+            }    
+        }
+
+        private void tbLastName_Validating(object sender, CancelEventArgs e)
+        {
+            // we can do any kind of validating 
+            if (tbLastName.Text.Length < 8)
+            {
+                e.Cancel = true; 
+                errorProvider1.SetError(tbLastName, "Last Name must be 8 or more chars");
+            }
+            else
+            {
+                e.Cancel = false; 
+                errorProvider1.SetError(tbLastName, ""); 
+            }
+        }
+    }
+}
