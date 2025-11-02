08c53513e5084cec566fee20b43ec5b5984c0587
Over <asz1095136568@gmail.com>
2025-11-03 02:21:04 +0300

    done

---
+++ Csharp-OOP/7 - First Concept of OOP- Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// same as C++ lessons
+// Encapsulation is to gather functions and variables and put them inside one container(class) and limit access to them by objects only.
+
+
+namespace _7___First_Concept_of_OOP__Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
