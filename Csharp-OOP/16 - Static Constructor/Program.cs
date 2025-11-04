376ca664d789eab2318ffea61c61f3deffe805bd
Over <asz1095136568@gmail.com>
2025-11-04 07:32:09 +0300

    done

---
+++ Csharp-OOP/16 - Static Constructor/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// static means on the class level , not the objects level
+// static constructor is is called once in the life of the program, unlike non static ones, called with each object.
+// static constructors cant be parameterized
+// We can have only one static constructor in a class. It cannot have any parameters or access modifiers.
+// We cannot call a static constructor directly. However, the static constructor gets called automatically when using memebrs of the static class
+
+namespace _16___Static_Constructor
+{
+
+    static class Settings
+    {
+
+        public static int DayNumber
+        {
+            get
+            {
+                return DateTime.Today.Day;
+            }
+        }
+
+
+        public static string DayName
+        {
+            get
+            {
+                return DateTime.Today.DayOfWeek.ToString();
+            }
+        }
+
+
+        public static string ProjectPath { set; get; }
+
+
+        // static constructors cant be parameterized
+        static Settings()
+        {
+            ProjectPath = "cckckck";
+            Console.WriteLine("im static constructor");
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("not using static class members, so static constructor not called yet");
+            Console.WriteLine("still not used");
+            Console.WriteLine("used in next lines");
+            Console.WriteLine(Settings.DayName);
+            Console.WriteLine(Settings.ProjectPath);
+
+
+        }
+    }
+}
---
+++ Csharp-OOP/17 - Destructor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17___Destructor
+{
+
+    class clsPerson
+    {
+
+        public clsPerson()
+        {
+            Console.WriteLine("constructor called");
+        }
+
+        // destructor must have telda (~)
+        // no modifiers needed, no parameters, no direct calling, called once object is destructed
+        ~clsPerson()
+        {
+            Console.WriteLine("destructor called");
+        }
+
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsPerson person1 = new clsPerson();
+
+            Console.WriteLine("while using te object, still destructor not called, once we finish, the garbage collector will destruct the object");
+            Console.WriteLine("then destructor will run, press any key to indicate that we finished with the object");
+            Console.ReadKey();
+        }
+    }
+}
