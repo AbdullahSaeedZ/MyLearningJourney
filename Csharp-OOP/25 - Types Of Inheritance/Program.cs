3c916f6f26d12a410c1dc388ba9f6a161170b7c8
Over <asz1095136568@gmail.com>
2025-11-05 11:55:02 +0300

    done

---
+++ Csharp-OOP/25 - Types Of Inheritance/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+
+1) Single Inheritance:
+
+   A
+   |
+   B
+
+
+2) Multi - Level Inheritance:
+
+A
+|
+B
+|
+C
+
+
+3) Hierarchical Inheritance:
+       A
+     / | \
+    B C  D
+
+
+4) Multiple Inheritance (Class → Class) - Not allowed in C#:
+
+   A     B
+    \   /
+      C
+
+
+5) Hybrid Inheritance :
+hybrid is mix of types
+since this has multiple which is not allowed, this type of hybrid is not allowed, other types of hybrid can be achieved
+ Hierarchical + Multiple
+
+       A
+     /   \
+    B     C
+     \   /
+       D
+
+*/
+
+namespace _25___Types_Of_Inheritance
+{
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
