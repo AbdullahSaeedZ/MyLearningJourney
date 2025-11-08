4466b6ff8fa4ef28c407a153169dfe134ce9f82d
Over <asz1095136568@gmail.com>
2025-11-08 21:57:00 +0300

    done

---
+++ Csharp-OOP/40 - protected internal/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+
+using MyLibrary;
+
+
+
+
+
+namespace _40___protected_internal
+{
+
+    class Student : Person   //  inheritance works from another assembly because Person is public, if Person was internal then we couldnt access it here
+    {
+        public void Test()
+        {
+            Speak();   //  allowed because of protected internal, same if it was only protected
+        }
+    }
+
+    class Human
+    {
+
+        public void method()
+        {
+            // not inheriting Person, so i cant reach the speak method, even though the class is public, but the method is protected internal
+          //  Speak(); 
+        }
+
+    }
+
+    public class Class1
+    {
+        internal void main(string[] args)
+        {
+
+            Student s = new Student();
+            s.Test();
+            s.greet(); // this is accessible cuz method is public and class is public 
+         // s.Speak() // cant access it, eventhough the base class is public in the other assembly, but the method itself is protected internal
+            
+
+        }
+
+
+    }
+}
---
+++ Csharp-OOP/protected/testClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// protected internal = protected + internal
+
+// Inside the same assembly:
+// - It behaves like public (any class in the same DLL/EXE can access it)
+
+// Outside the assembly:
+// - It behaves like protected (only accessible through inheritance)
+
+
+
+namespace MyLibrary
+{
+    public class Person   // ← must be public to allow inheritance from other projects
+    {
+
+        // this will be accessible outside assembly for only classes that inherits this class, and here in the assembly will be public (accessed from objects and anywhere)
+        protected internal void Speak()   // ← accessible only through inheritance outside the assembly
+        {
+            Console.WriteLine("Hello");
+        }
+
+        // this will be accessible from any where in the other assembly 
+        public void greet()
+        {
+            Console.WriteLine("greeting");
+        }
+    }
+
+
+    public class boy
+    {
+
+        public void method()
+        {
+            Person p1 = new Person();
+            p1.Speak(); // it is accessed here, it acts as public, but outside the assembly it is protected for inherited classes only
+        }
+    }
+}
