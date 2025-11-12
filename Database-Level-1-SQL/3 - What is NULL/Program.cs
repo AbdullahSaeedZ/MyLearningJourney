bf517c8bbc8886972cca12474ecbc63cd5c510ac
Over <asz1095136568@gmail.com>
2025-11-13 01:48:49 +0300

    done

---
+++ Database-Level-1-SQL/3 - What is NULL/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// null is a special marker used to indicate that data value does not exist in the data base
+// it represent a missing or unknown value in a table column
+
+// Null = nothing = no value
+// Null != 0
+// null != empty
+// null might mean not yet defined or missing 
+// for example, i have a table with employees and their info, but one employee is missing his birthday date or i will add it later,
+// so instead of putting any value to make it work, we use null
+
+// another ex, a father in the table and i dont know how many kids he has, if i put zero, that means i know he has zero kids, but i actually dont know how many kids yet! so i use null.
+
+// or if a field is optional, i use null
+
+namespace _3___What_is_NULL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
