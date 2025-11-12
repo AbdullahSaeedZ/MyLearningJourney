df4c8668194569a3da1fb5bb480375c849406598
Over <asz1095136568@gmail.com>
2025-11-12 22:16:17 +0300

    done

---
+++ Database-Level-1-SQL/2 - What is Database/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// A database is an organized collection of data so that it can be easily accessed.To manage these databases, Database Management Systems (DBMS) are used
+
+// Types of DBMS:
+
+// Non-Relational (DBMS): Stores data without defined relationships between entities.
+// Data is scattered or loosely organized with no strict structure or classification.
+// Examples: File System, XML, JSON, etc.
+
+// Relational (RDBMS): enhanced version of DBMS that supports relations between tables.
+// Examples SQLServer, Oracle, MySQL ..et
+
+
+// DBMS and RDBMS are not the same.
+
+// DBMS stands for Database Management Systems
+
+// Dealing with RDBMS is much easier than dealing with DBMS.
+
+// RDBMS: Data that is stored in an organized fashion in tables containing rows and columns along with relations between these tables.
+
+// Column/Field/Attribute are the same.
+
+// Row/Entity/Record are the same
+
+// Table/Entity Set are the same.
+
+// Relationships are represented using references to data from other tables.
+
+// IMPORTANT : check pdf and ipad, full info
+
+
+namespace _2___What_is_Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
