2e04b23bc884efb4a1ac45310545cf4749253c47
Over <asz1095136568@gmail.com>
2025-11-16 13:15:20 +0300

    done

---
+++ Database-Level-1-SQL/11 - ERD Symbols/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// IMPORTANT: see pdf and Notebooklm
+
+// Composite Attribute: attribute that has sub attributes
+// Derived Attribute: attribute that can be calculated or derived from available attributes like full name from first and last name, but not included in the table
+// Underscore for primary key: is called Key Attribute
+
+// Entity or strong entity (has primary key): rectangle
+// X Weak entity (no primary key, best to never use): double rectangles
+// Lines
+// Relations: Diamonds
+// attribute: circle or Ellipses (بيضاوي)
+// X Multi-valued attribute (fields that can represent two things, best to never use): Double circles 
+// Derived attributes: Dashed circle
+// Composite attributes: Circles or Ellipses
+// Key attribute: Circle with underscored name
+
+
+
+
+
+namespace _11___ERD_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Database-Level-1-SQL/11 - ERD Symbols/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("11 - ERD Symbols")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("11 - ERD Symbols")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5d26568-a5a1-4f59-91fa-d20c6a06d33c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
