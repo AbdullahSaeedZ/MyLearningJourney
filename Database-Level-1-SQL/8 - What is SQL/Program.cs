f44751992b3254893a4bc1762ca24ec578230c1b
Over <asz1095136568@gmail.com>
2025-11-16 08:42:44 +0300

    done

---
+++ Database-Level-1-SQL/8 - What is SQL/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// SQL = Structured Query Language
+// sometimes pronounced as See-Quel.
+
+// SQL is used to communicate with a data base
+// it lets us access and manipulate data bases
+
+// SQL is a standard language that will work with different DBMS (but very small syntax difference sometimes) like:
+// Oracle, Sybase, Microsoft SQL Server, Access, Ingers, etc.
+
+
+
+
+// Types of SQL statements (SQL Sub-languages)
+
+// 1) Data Definition Language (DDL)
+// Definition = structure of data bases.
+// Used to define or modify database objects.
+// Examples: CREATE, ALTER, DROP, TRUNCATE.
+
+// 2) Data Manipulation Language (DML)
+// Manipulation = change the data itself.
+// Used to INSERT new data, UPDATE existing data, DELETE data.
+
+// 3) Data Control Language (DCL)
+// Control = permissions.
+// Used to manage user access.
+// Examples: GRANT (give access), REVOKE (remove access).
+
+// 4) Transaction Control Language (TCL)
+// Transaction = confirm or undo changes.
+// Controls the behavior of transactions.
+// Examples: COMMIT (save), ROLLBACK (undo), SAVEPOINT (partial rollback).
+
+// 5) Data Query Language (DQL)
+// Query = fetching data.
+// Only one main command: SELECT.
+
+
+// see pdf
+
+
+
+namespace _8___What_is_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
