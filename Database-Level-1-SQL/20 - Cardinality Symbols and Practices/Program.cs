20ce0cc1de237037196ba7b5b2a7a8e65a7e0786
Over <asz1095136568@gmail.com>
2025-11-20 19:33:45 +0300

    done

---
+++ Database-Level-1-SQL/20 - Cardinality Symbols and Practices/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// the common way is to use Cardinality Notation Symbols
+// see pdf and ipad, very very very important
+
+
+
+namespace _20___Cardinality_Symbols_and_Practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
