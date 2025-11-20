9dbe8ff1c60abfb38bdafd79d5ce9f91406d2cd8
Over <asz1095136568@gmail.com>
2025-11-20 20:33:29 +0300

    done

---
+++ Database-Level-1-SQL/22 - Process of Creating ERD Step by Step/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// see pdf for requirement
+
+// Steps to create ERD:
+
+// 1- Entity Identification:
+// identify entities of the system.
+
+// 2- Relationship Identification:
+// identify the relationship among all entities, whether there is a relation or not
+
+// 3- Cardinality Identification:
+// identify each relation type, this includes the ordinality
+
+// 4- Identify Attributes of Entities
+
+// 5- Create ERD
+
+
+namespace _22___Process_of_Creating_ERD_Step_by_Step
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
