5ef5b9ff29da2166fe84fc2c5aab390bba39fb1e
Over <asz1095136568@gmail.com>
2025-11-20 19:43:27 +0300

    done

---
+++ Database-Level-1-SQL/21 - Total Vs Partial Participation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// see vid and pdf
+
+// total and partial participation is another way of describing relationship of entities
+
+// total = mandatory = min 1 
+// partial = optional = min 0 
+
+namespace _21___Total_Vs_Partial_Participation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
