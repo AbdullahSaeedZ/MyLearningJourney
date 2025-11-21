b7484c96f63424a761613cd5a7b0e5a67004cfbf
Over <asz1095136568@gmail.com>
2025-11-21 21:09:47 +0300

    done

---
+++ Database-Level-1-SQL/26 - What is Relational Schema/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// A relational schema is a set of relational tables and associated items that are related to one another.
+// Relation schema defines the design and structure of the relation
+// like it consists of the relation name, set of attributes/field names/column names. every attribute would have an associated domain.
+
+// once we gather requirement of the database, we create  the ERD which is a conceptual design
+// once ERD is ready we convert it into a Relational Schema which is a part of Logical design of data base
+// Relational Schema translates ERD into tables and more details to be ready for building the data base
+
+// see pdf for pics and examples
+
+
+namespace _26___What_is_Relational_Schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
