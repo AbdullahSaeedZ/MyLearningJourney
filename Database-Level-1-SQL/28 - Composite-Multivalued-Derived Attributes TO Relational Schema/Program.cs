c36877c14db03e061d081dec6ac29b3133130ea6
Over <asz1095136568@gmail.com>
2025-11-21 23:51:18 +0300

    done

---
+++ Database-Level-1-SQL/28 - Composite-Multivalued-Derived Attributes TO Relational Schema/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28___Composite_Multivalued_Derived_Attributes_TO_Relational_Schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
