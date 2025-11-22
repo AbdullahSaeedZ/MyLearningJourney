f1739c2c9e3a41eed83cfa4c8e1e22abc6f6776b
Over <asz1095136568@gmail.com>
2025-11-23 02:47:00 +0300

    done

---
+++ Database-Level-1-SQL/29 - Convert One-to-One TO Relational Schema/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+// see diagram file
+
+
+
+namespace _29___Convert_One_to_One_TO_Relational_Schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
