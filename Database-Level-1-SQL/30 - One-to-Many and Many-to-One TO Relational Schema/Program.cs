d2140cb7db5bd93206af6532f572b6eedd4364a7
Over <asz1095136568@gmail.com>
2025-11-23 03:30:54 +0300

    done

---
+++ Database-Level-1-SQL/30 - One-to-Many and Many-to-One TO Relational Schema/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// diagram file
+
+namespace _30___One_to_Many_and_Many_to_One_TO_Relational_Schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
