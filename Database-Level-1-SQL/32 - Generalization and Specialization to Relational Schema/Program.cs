3adeb54575cd87234b199b67bcd3d38b76c48318
Over <asz1095136568@gmail.com>
2025-11-24 05:31:21 +0300

    done

---
+++ Database-Level-1-SQL/32 - Generalization and Specialization to Relational Schema/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// diagram
+
+
+namespace _32___Generalization_and_Specialization_to_Relational_Schema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
