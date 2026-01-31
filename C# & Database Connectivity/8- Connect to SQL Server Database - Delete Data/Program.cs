ba95c7aa8e8c0464f847e79f92a1f36b2d7a53cb
Over <asz1095136568@gmail.com>
2026-01-31 16:50:30 +0300

    done

---
+++ C# & Database Connectivity/8- Connect to SQL Server Database - Delete Data/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+
+public class Program
+{
+
+    static string connectionString = "Server=.;Database=ContactsDB;User Id=sa;Password=123456;";
+
+    class clsInfo
+    {
+        public int ContactID { set; get; }
+        public string FirstName { set; get; }
+        public string LastName { set; get; }
+        public string Email { set; get; }
+        public string Phone { set; get; }
+        public string Address { set; get; }
+        public int CountryID { set; get; }
+
+        public clsInfo(string FirstName, string LastName, string Email, string Phone, string Address, int CountryID)
+        {
+            // this.ContactID = ContactID;   // auto increment in database
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.CountryID = CountryID;
+        }
+
+        public void deleteRecordFromDatabase()
+        {
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = @"delete from Contacts
+                             where ContactID = @ID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ID", this.ContactID);
+
+            try
+            {
+                connection.Open();
+                int rowAffected = command.ExecuteNonQuery();  // non query, cuz we arent querying, we are updating, this method returns int of how many affected rows
+
+                if (rowAffected > 0)
+                    Console.WriteLine("Record Deleted!");
+                else
+                    Console.WriteLine("Record was not Deleted!");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+
+    }
+
+
+
+    static void Main(string[] args)
+    {
+
+        clsInfo Contact = new clsInfo("Abdullah", "Alzahrani", "AZ@AZ.com", "054833", "Dmm st1", 2);
+
+        // assign id to look for and delete, or make it as parameter, or any way u like
+        Contact.ContactID = 9;
+        Contact.deleteRecordFromDatabase();
+
+    }
+}
