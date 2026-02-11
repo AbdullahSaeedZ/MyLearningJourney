54ef8b7876899b4bc3e104754754fbe782cf7939
Over <asz1095136568@gmail.com>
2026-02-11 05:35:40 +0300

    done

---
+++ C# & Database Connectivity/18- Datatable Example 4 (Sorting)/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+
+// sorting in DataTable is slow cuz it has to reorder the original table in the memory by deleting and adding rows manually
+// sorting in DataView is faster cuz it will just point to the original rows indexes in different order to sort
+
+// DataTable holds the original data.
+// The order of rows is fixed based on insertion and does NOT change when sorting.
+
+// DataView is a view layer on top of the DataTable.
+// It acts like a lens that shows the same data in a different order.
+
+// Sorting does NOT move or reorder rows inside the DataTable.
+// Sorting only rearranges indexes that point to the original rows.
+
+// If you want to actually change the physical order of rows in DataTable.Rows:
+// 1) Create a new DataTable and insert rows in the desired order
+// OR
+// 2) Clear the table and reinsert rows in the new order
+
+// Otherwise, the original row order in the DataTable always remains unchanged.
+
+
+namespace _15__DataTable_Example_1__Create_Offline_Data_Table_and_ListData_
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            DataTable Employees = new DataTable();
+
+            // define the columns
+            Employees.Columns.Add("ID", typeof(int));
+            Employees.Columns.Add("FirstName", typeof(string));
+            Employees.Columns.Add("LastName", typeof(string));
+            Employees.Columns.Add("Country", typeof(string));
+            Employees.Columns.Add("Salary", typeof(double));
+            Employees.Columns.Add("Date", typeof(DateTime));
+
+            // now we add the recoقds
+            Employees.Rows.Add(1, "Abdullah", "Alzahrani", "KSA", 24000, DateTime.Now);
+            Employees.Rows.Add(2, "Fawaz", "Alzahrani", "KSA", 24054, DateTime.Now);
+            Employees.Rows.Add(3, "koko", "Alkoko", "Egypt", 22500, DateTime.Now);
+            Employees.Rows.Add(4, "fofo", "Alkfoo", "Lebanon", 5300, DateTime.Now);
+            Employees.Rows.Add(5, "dodo", "Aldooo", "USA", 2300, DateTime.Now);
+
+            //-----------------------------------------------------------------------------------------
+            // Normal Table
+
+            Console.WriteLine("Employees List:\n");
+            Console.WriteLine(" {0, -2} | {1, -10} | {2, -10} | {3, -10} | {4, -10} | {5, -10}", "ID", "FirstName", "LastName", "Country", "Salary", "Date");
+            foreach (DataRow record in Employees.Rows)
+            {
+                Console.WriteLine(" {0, -2} | {1, -10} | {2, -10} | {3, -10} | {4, -10} | {5, -10}", record["ID"], record["FirstName"], record["LastName"], record["Country"], record["Salary"], record["Date"]);
+            }
+
+
+          
+
+            // sort by ID Desc:
+            Employees.DefaultView.Sort = "ID desc";
+            Employees = Employees.DefaultView.ToTable();
+
+            Console.WriteLine("\nEmployees List sorted by ID Desc:\n");
+            foreach (DataRow record in Employees.Rows)
+            {
+                Console.WriteLine(" {0, -2} | {1, -10} | {2, -10} | {3, -10} | {4, -10} | {5, -10}", record["ID"], record["FirstName"], record["LastName"], record["Country"], record["Salary"], record["Date"]);
+            }
+
+
+            /*
+               Employees is the name of the original table
+               DefaultView is not a table—it's just a view
+               ToTable() creates a new DataTable
+               The variable Employees now points to that new table
+               The original table disappears because it no longer has any reference pointing to it (By garbage Collector)
+            */
+
+
+            // sort by FirstName asc:
+            Employees.DefaultView.Sort = "FirstName asc";
+            Employees = Employees.DefaultView.ToTable();
+
+            Console.WriteLine("\nEmployees List sorted by FirstName asc:\n");
+            foreach (DataRow record in Employees.Rows)
+            {
+                Console.WriteLine(" {0, -2} | {1, -10} | {2, -10} | {3, -10} | {4, -10} | {5, -10}", record["ID"], record["FirstName"], record["LastName"], record["Country"], record["Salary"], record["Date"]);
+            }
+
+        }
+    }
+}
---
+++ C# & Database Connectivity/18- Datatable Example 4 (Sorting)/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("18- Datatable Example 4 (Sorting)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("18- Datatable Example 4 (Sorting)")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe63f418-60d3-4e5b-a3e2-2332383aeb5e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
