6abcaee2d991635c46b57a503ba2a7d40d59ff61
AbdullahSaeedZ <asz1095136568@gmail.com>
2026-05-30 19:09:03 +0300

    Delegates recap + null conditional operator with delegates

---
+++ C# Programming Level 2/2- Delegates and Events/10- Null-conditional Operator with Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyConsoleApp
+{
+    internal class Program
+    {
+        public static event Action<int> OnPersonSelected;
+
+        private static void Main(string[] args)
+        {
+            // there are two issues of invoking an event with the normal way:
+            // OnPersonSelected.Invoke(5);  // un comment and run to see the nullReferenceException
+
+            // first is to invoke an event with no methods subscribed, meaning the delegate is null
+            // second is Race Condition issue that happens in multithreading apps,
+            // where one thread invokes an event, and in the same time another thread unsubscribe a method from that event which will cause null reference exception
+
+
+            // to avoid this both situations, we use Null-Conditional operator (?)
+            // which will avoid invoking null delegates, check DVLD where i used events
+            OnPersonSelected?.Invoke(5);
+
+
+            // this will run without using the null operator, cuz there is aleardy a subscribed method
+            OnPersonSelected += PrintPersonID;
+            OnPersonSelected.Invoke(5);
+
+        }
+
+        public static void PrintPersonID(int ID)
+        {
+            Console.WriteLine("Person ID is: " + ID);
+        }
+
+    }
+}
---
+++ C# Programming Level 2/2- Delegates and Events/5- Generic Delegates/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+// generic = template
+
+namespace _5__Generic_Delegates
+{
+
+
+    // declare one generic delegate:
+    delegate T delGeneric<T>(T x, T y);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // purpose is to have one delegate definition that can declare for multiple delegate instances to reference to multiple methods
+            delGeneric<int> delSum = (int x, int y) => x + y;
+            Console.WriteLine(delSum(2,5));
+
+            delGeneric<string> delConcat = (string a, string b) => a + " " + b;
+            Console.WriteLine(delConcat("Abdullah", "Alzahrani"));
+
+        }
+    }
+}
