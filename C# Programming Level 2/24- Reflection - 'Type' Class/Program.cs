f7d182f1b3e07a6d0bec8c4fb93de1a7f48dad94
AbdullahSaeedZ <asz1095136568@gmail.com>
2026-06-12 13:49:35 +0300

    instantiating objects at run time using reflection

--- C# Programming Level 2/24- Reflection - 'Type' Class/Program.cs
+++ C# Programming Level 2/24- Reflection - 'Type' Class/Program.cs
@@ -25,68 +25,127 @@
      * queries these tables and returns a 'System.RuntimeType' instance (inheriting 'Type') 
      * filled with pointers to that class's layout in memory.
      */
-    public class TestClass
-    {
-        public int age { get; set; }
-        public string name { get; set; }
-        private void testvoid()
-        {
-        }
-        public void testvoid1()
-        {
-        }
-    }
+ 
 
     internal class Program
     {
         // The Type class is a central class in reflection, representing a type in C#.
         // we use it to get and navigate the information about a type, such as its methods, properties, fields, and events.
-        
+
+        Type type0 = typeof(Employee);
+        // typeof(Employee); <- this will return a reference to an object of RuntimeType class (hidden by microsoft) which has memebers that we use like name, fullname, IsClass
+        // the CLR will take the metadata after this file is compiled into exe file + metadata, then since we used reflection on Employee, and Employee metadata is available
+        // the CLR will query the metadata table and store the data in a RuntimeType object on heap.
+        // then the RuntimeType object is upcasted to its base class Type, the reasons of upcasting to the base class Type are:
+
+        //1. Encapsulation & Security: 'System.RuntimeType' is an internal, implementation-specific class. 
+        //   Microsoft hides it so developers cannot access internal CLR methods or break runtime memory management,
+        //   so they give us the base class Type as a reference variable to access certain memebrs of subclass RuntimeType.
+
+        // 2. Unified API (Polymorphism): 'Employee' isn't the only thing that needs a descriptive object of RuntimeType class.
+        //    .NET handles completely different types of structures in memory, such as Dynamic Types or external COM Objects.
+        //    Each of these has its own specific, hidden internal descriptive class written completely differently from 'RuntimeType'.
+        //    
+        //    This is where the magic of Upcasting comes in: since all these diverse internal catalog classes are forced 
+        //    to inherit from the shared abstract 'System.Type' base class, you can use a single 'Type' reference variable 
+        //    to store and interact with ANY type in .NET using the exact same methods and properties, without caring 
+        //    about the underlying concrete implementation.
+
+        //3. Backward Compatibility & Flexibility: It decoupling the public API from the internal engine. 
+        //    Microsoft can completely rewrite how 'RuntimeType' works internally in future .NET versions, 
+        //    but your code won't break because your code only depends on the stable 'System.Type' base class.
+
+
+        public static object GetUnknownType()
+        {
+            string input = "4";
+
+            if (int.TryParse(input, out int number))
+            {
+                // we check if user entered a number then return an int object at run time
+                return number;
+            }
+            else
+            {
+                // we check if user didnt enter a number then return a string object at run time
+                return "";
+            }
+        }
+
         static void Main(string[] args)
         {
-            // 
-            Type type = typeof(string);
+            // ======================================================= ++++ Obtaining Types ++++ ======================================================= \\
 
+
+            // getting metadata of DateTime class from the assembly it was defined in, then return a RuntimeType object filled with info upcasted to a Type class
+
+            // 1- At runtime,
+            // if we dont know what objects will be sent to us to check, we initially define an object data type, which is a base for all data types
+            // then we use GetType to get the actual type in runtime:
+            object unknown = GetUnknownType();
+            Type typeTest = unknown.GetType(); // if user passes a string, then this will be a string Type
+
+            // 2- At compile time,
+            // we already know the objects when writing the code, so we use typeof
+            Type type = typeof(DateTime);
             Console.WriteLine("the Type info:");
             Console.WriteLine($"type name: {type.Name}");
             Console.WriteLine($"type full name: {type.FullName}");
             Console.WriteLine($"is this type a class?: {type.IsClass}");
 
-
-            // or any class such as this one i created above
+            // or any class
             Type type1 = typeof(int);
-
             Console.WriteLine("\n\nthe Type1 info:");
-            Console.WriteLine($"type name: {type1.Name}");
-            Console.WriteLine($"type full name: {type1.FullName}");
+            Console.WriteLine($"type name: {type1.Name}");// returns TypeName
+            Console.WriteLine($"type name: {type1.Namespace}");// returns Namespace
+            Console.WriteLine($"type full name: {type1.FullName}"); // returns Namespace.TypeName
+            Console.WriteLine($"type full name: {type1.BaseType}"); // returns base type where int is inheriting, value type
             Console.WriteLine($"is this type a class?: {type1.IsClass}");
 
 
-            // 
-            Type type2 = typeof(TestClass);
-            // typeof(TestClass); <- this will return a reference to an object of RuntimeType class (hidden by microsoft) which has memebers that we use like name, fullname, IsClass
-            // the CLR will take the metadata after this file is compiled into exe file + metadata, then since we used reflection on TestClass, and TestClass metadata is available
-            // the CLR will query the metadata table and store the data in a RuntimeType object on heap.
-            // then the RuntimeType object is upcasted to its base class Type, the reasons of upcasting to the base class Type are:
+            // =================  Obtaining info of nested types, example is classes defined inside a class (nested):
+            Type employeeType = typeof(Employee);
+            Console.WriteLine($"\n\nEmployee Type Name: {employeeType.Name}");
 
-            //1. Encapsulation & Security: 'System.RuntimeType' is an internal, implementation-specific class. 
-            //   Microsoft hides it so developers cannot access internal CLR methods or break runtime memory management,
-            //   so they give us the base class Type as a reference variable to access certain memebrs of subclass RuntimeType.
+            // must be in an array to store all nested types of class Employee
+            Type[] nestedTypes = typeof(Employee).GetNestedTypes();
 
-            // 2. Unified API (Polymorphism): 'TestClass' isn't the only thing that needs a descriptive object of RuntimeType class.
-            //    .NET handles completely different types of structures in memory, such as Dynamic Types or external COM Objects.
-            //    Each of these has its own specific, hidden internal descriptive class written completely differently from 'RuntimeType'.
-            //    
-            //    This is where the magic of Upcasting comes in: since all these diverse internal catalog classes are forced 
-            //    to inherit from the shared abstract 'System.Type' base class, you can use a single 'Type' reference variable 
-            //    to store and interact with ANY type in .NET using the exact same methods and properties, without caring 
-            //    about the underlying concrete implementation.
-
-            //3. Backward Compatibility & Flexibility: It decoupling the public API from the internal engine. 
-            //    Microsoft can completely rewrite how 'RuntimeType' works internally in future .NET versions, 
-            //    but your code won't break because your code only depends on the stable 'System.Type' base class.
+            for (byte i = 0; i < nestedTypes.Length; i++)
+            {
+                Console.WriteLine($"Nested type Name of index {i}: {nestedTypes[i].Name}");
+            }
 
 
+            // =================  Obtaining info about interfaces inherited by a certain type:
+
+            Type[] interfacesOfType = typeof(int).GetInterfaces();
+
+            Console.WriteLine("\n\nInterfaces inherited by Int type");
+            for (byte i = 0; i < interfacesOfType.Length; i++)
+            {
+                Console.WriteLine($"Interface Name:{interfacesOfType[i].Name}");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string name { get; set; }
+
+        // this is composition, will not be showm in the nestedTypes
+        public FullTimeEmployee fullTime  = new FullTimeEmployee();
+
+        // this is nested class, will be shown in nestedTypes
+        public class FullTimeEmployee
+        {
+        }
+
+        public class PartTimeEmployee
+        {
+        }
+
+        public void testvoid1()
+        {
         }
     }
 }
---
+++ C# Programming Level 2/25- Reflection - Instantiating Types/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Reflection;
+using System.Runtime.Remoting;
+
+namespace TestProject
+{
+    // ============================================ instantiating objeccts using Types and reflection ============================================ \\
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate an integer:
+            int i = new int(); // default of 0
+            i = 5;
+
+            // another way to instantiate an integer is by using Reflection:
+            // we use Activator class, which takes a type as parameter and returns an object of Object data type then we explicitly cast it as needed:
+            int a = (int)Activator.CreateInstance(typeof(int));
+            a = 3;
+
+            // but the issue is that Activator.CreateInstance(typeof(int)) will return an Object data type, which will be stored in heap with default of 0 value
+            // and that is called boxing, where we box a value type in an object in heap, then we unbox by casing to int to store it in stack
+            // that is overhead and a waste of performance, solution is a new approach of returning a generic T Type directly,not an object in heap, unless T is a reference type:
+            // - if T is a value type (like int): it constructs the instance directly on the Stack (no heap allocation,No Boxing)
+            // - if T is a reference type (like class): it instantiates it on the heap normally
+            int newApproach = Activator.CreateInstance<int>();
+
+
+
+            //   THE TRADE-OFF (Generics vs Old method with casting)
+            // - Activator.CreateInstance<T>() is highly optimized but limited to types known at compile-time 
+            //   and primarily handles default (parameterless) constructors.
+            // - The Non-Generic Activator can face the boxing/unboxing overhead buy it has many overloads allowing ultimate flexibility at runtime, 
+            //   such as passing constructor arguments, invoking private constructors, or instantiating types via strings.
+
+            // another way to instantiate a reference type with parameterless ctor:
+            object emp = Activator.CreateInstance(typeof(Employee));
+
+            // or with casting:
+            Employee emp1 = (Employee)Activator.CreateInstance(typeof(Employee));
+            Console.WriteLine(emp1);
+
+            // another way to instantiate a reference type with parameterized ctor using another overload in Activator:
+            // Example of Old Approach flexibility (Passing parameters to a constructor at runtime):
+            object[] constructorArgs = { "Ahmed", 2 }; // must be same order in the constructor
+            Employee empWithParameters = (Employee)Activator.CreateInstance(typeof(Employee), constructorArgs);
+            Console.WriteLine(empWithParameters);
+
+
+            // ====================================== instantiating objects dynamically at run time =================================== \\
+
+            // Example: A player in a game chooses a monster by name, then info will be shown.
+            // Idea: Create certain objects dynamically at runtime by passing the class name as a string using Reflection.
+
+
+            // Scenario 1: Using Activator with strings. This provides isolation capabilities (ObjectHandle). 
+            // It is ideal if we are instantiating objects inside a separate/custom AppDomain (or AssemblyLoadContext in modern .NET)
+            // so we can interact through a handle and eventually unload the entire domain/context to free memory.
+            Console.WriteLine("\n\nEnter Name of Monster [Pixa - Toba - Meno]:");
+
+            // the dll name will be dynamiclly recieved as string, also the class name which we will use to create the object
+            string externalAssemblyName = "TestProject";
+            string userInput = $"{externalAssemblyName}." + Console.ReadLine();
+
+            // The Activator will look for the assembly using a searching method called the Probing Process (Application directory, shared runtimes, and configured dependency paths)
+            // It returns an ObjectHandle (a wrapper), which keeps the object isolated until we explicitly unwrap it to our current domain context.
+            ObjectHandle chosenMonsterHandle = Activator.CreateInstance(externalAssemblyName, userInput);
+
+            // Unwrap grants our current domain direct access to the object reference in the Heap.
+            object chosenMonsterObj = chosenMonsterHandle.Unwrap();
+            Console.WriteLine(chosenMonsterObj);
+
+
+
+            // Scenario 2: Direct local instantiation using an Assembly object that is already active and loaded in our current AppDomain.
+            Console.WriteLine("\n\nEnter Name of Monster [Pixa - Toba - Meno]:");
+            string userInput2 = $"TestProject." + Console.ReadLine();
+
+            // We already have the Assembly object in memory; we just lookup its metadata directly and instantiate immediately.
+            Assembly gameAssembly2 = typeof(Program).Assembly;
+            object chosenMonster2 = gameAssembly2.CreateInstance(userInput2); // Returns object directly, no handles, no unwrapping needed.
+            Console.WriteLine(chosenMonster2);
+
+
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; } = "Abdullah";
+        public int Age { get; set; } = 1;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string Name, int Age)
+        {
+            this.Age = Age;
+            this.Name = Name;
+        }
+
+        public override string ToString()
+        {
+            return $"Employee Name: {Name}, Age: {Age}";
+        }
+    }
+
+    // monsters
+    public class Pixa
+    {
+        public override string ToString()
+        {
+            return $"Monster Pixa is choosen with Power: {55}, Damage: {99}";
+        }
+    }
+
+    public class Toba
+    {
+        public override string ToString()
+        {
+            return $"Monster Toba is choosen with Power: {82}, Damage: {43}";
+        }
+    }
+
+    public class Meno
+    {
+        public override string ToString()
+        {
+            return $"Monster Meno is choosen with Power: {63}, Damage: {74}";
+        }
+    }
+}
+
+
