59794533939275a6fea6835ecfa173893564caf9
AbdullahSaeedZ <asz1095136568@gmail.com>
2026-06-18 13:19:08 +0300

    events logging lessons

---
+++ C# Programming Level 2/43- Logging - What is Event Log/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+In computing, an event log is a file or database used to store events that occur in a system.
+These events can include information about errors, warnings, system events, user activities, and more. 
+Event logs are commonly used for troubleshooting, monitoring, and auditing purposes.
+
+we can use it once exceptions are thrown in a try-catch block, even if we show the error message to the user,
+but we need to log the error for trouleshooting and improvements
+
+To run the Windows Event Viewer, you can follow these steps:
+
+1-Open the Start Menu:
+In Windows 10, you can click on the Windows icon in the bottom left corner of your screen.
+
+2-Search for "Event Viewer":
+Type "Event Viewer" into the search bar.
+
+3-Open Event Viewer:
+Click on the "Event Viewer" app that appears in the search results.
+
+4-Navigate to Event Logs:
+In the Event Viewer window, you'll see a left-hand navigation pane. Click on "Windows Logs" to expand it.
+
+5-Choose a Log:
+Under "Windows Logs," you'll find several logs like "Application," "Security," "Setup," "System," and more. 
+Choose the log you're interested in viewing. For the example provided earlier, you would likely find the log entry in the "Application" log.
+
+6-View Log Entries:
+Click on the log you selected, and you'll see a list of log entries in the center pane. Look for the entry created by your C# program.
+The logs in the Event Viewer contain information about various events on your system, including system errors, warnings, information events, and more. 
+You can filter, sort, and search for specific events within the Event Viewer interface to help you locate the information you need.
+
+
+After running this program, you can check the Windows Event Viewer to see the log entry. 
+Open the Event Viewer, navigate to "Windows Logs," and select "Application" to view the log entries.
+ 
+ */
---
+++ C# Programming Level 2/44- Logging - Logging Example/Program.cs
@@ -0,0 +1,50 @@
+﻿//add this class through NuGet packages:  System.Diagnostics.EventLog
+using System.Diagnostics; 
+
+namespace _44__Logging___Logging_Example
+{
+    // i will store my app event logs in the system event viewr (the centralized database) so i can track processes or errors.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1- we preapre the event log info:
+            string sourceName = "testApp"; // specify the source, the name of the app
+            string logName = "Application"; //specify which folder(Application, Security..)
+
+            // needs to be run as adminstrator or add the manifest file
+            try
+            {
+                //2- create the event source if not created
+                if (!EventLog.SourceExists(sourceName))
+                {
+                    EventLog.CreateEventSource(sourceName, logName);
+                    Console.WriteLine($"source name {sourceName} is created in {logName} log");
+                }
+
+
+                // for example: those can be used in try-catch blocks in my DVLD project to track errors,
+                // or to have any logs like when adding new people in the system or whatever process we need to track
+
+                // log an event of Information type
+                EventLog.WriteEntry(sourceName, "this is an information event: user logged in", EventLogEntryType.Information);
+
+                // log an event of Warning type
+                EventLog.WriteEntry(sourceName, "this is a warning event: user was blocked due to 3 failed log in attempts", EventLogEntryType.Warning);
+
+                // log an event of Error type
+                EventLog.WriteEntry(sourceName, "this is an Error event: (exception message) Database connection failed due to..", EventLogEntryType.Error);
+
+
+                Console.WriteLine("events logged successfully ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+        }
+    }
+}
